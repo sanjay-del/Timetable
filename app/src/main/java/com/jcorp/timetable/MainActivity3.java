2dae578b7a1a8c27970bec4a51c9a48f74409fb6
learner-1010 <sanjaypantha.np@gmail.com>
2019-03-02 21:44:09 +0545

    database-13

--- app/src/main/java/com/jcorp/timetable/MainActivity3.java
+++ app/src/main/java/com/jcorp/timetable/MainActivity3.java
@@ -1,7 +1,15 @@
 package com.jcorp.timetable;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -9,5 +17,8 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+
+
     }
 }
